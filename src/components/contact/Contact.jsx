6dfdce876266cfd2b848ab50459a08c724767908
luciamicheloud <123605664+luciamicheloud@users.noreply.github.com>
2023-11-18 17:16:48 -0300

    logos

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,20 +5,20 @@ const Contact = () => {
   return (
     <div className="contact-icons">
 
-        <a href="https://www.linkedin.com/in/lucia-micheloud-300324264/" target="_blank" rel="noopener noreferrer">
-          <img src="src/images/linkedin.png" alt="LinkedIn" />
+        <a href="micheloudrionlucia@gmail.com">
+          <img src="https://freelogopng.com/images/all_img/1657906274logo-gmail-png.png" alt="Gmail" />
         </a>
 
         <a href="https://github.com/luciamicheloud" target="_blank" rel="noopener noreferrer">
           <img src="https://t3.ftcdn.net/jpg/04/52/18/48/360_F_452184858_iZXfI1h2BdKB5i9cDwpu9Ue1il8Mxxfq.jpg" alt="GitHub" />
         </a>
 
-        <a href="ruta-instagram" target="_blank" rel="noopener noreferrer">
-          <img src="https://freepnglogo.com/images/all_img/1691832460x-twitter-logo-png.png" alt="twitter" />
+        <a href="https://www.linkedin.com/in/lucia-micheloud-300324264/" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" />
         </a>
 
-        <a href="micheloudrionlucia@gmail.com">
-          <img src="src/images/gmail.png" alt="Gmail" />
+        <a href="ruta-instagram" target="_blank" rel="noopener noreferrer">
+          <img src="https://freepnglogo.com/images/all_img/1691832460x-twitter-logo-png.png" alt="twitter" />
         </a>
 
     </div>
